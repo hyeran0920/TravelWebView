ec71feaef7de5041b05cb59fcc827570eddb5820
ujin <hyeran0920@naver.com>
2024-09-27 15:49:41 +0900

    Add Food

--- src/Component/RecomTravel.js
+++ src/Component/RecomTravel.js
@@ -32,7 +32,7 @@ const locations = [
 const RecomTravel = () => {
 
   const navigate = useNavigate();
-  
+
   const handleLocationClick = (locationName) => {
     // navigate로 지역명을 URL 파라미터로 전달
     navigate(`/locations/${locationName}`);
@@ -49,7 +49,7 @@ const RecomTravel = () => {
             style={{
               borderRadius: '10px',
               overflow: 'hidden',
-              height: '150px',
+              height: '180px',
               position: 'relative',
               display: 'flex',
               alignItems: 'flex-end',
@@ -66,11 +66,20 @@ const RecomTravel = () => {
               className="absolute inset-0 object-cover object-top w-full h-full rounded-lg"
             />
             <div className="relative z-10 top-2">
-              <div className="text-left text-white shadow-lg">{place.name}</div>
+              <div
+                style={{
+                  textShadow: '2px 2px 4px rgba(0, 0, 0, 0.5)',
+                  color: 'white',
+                  fontSize: '18px',
+                  fontWeight: 'bold'
+                }}
+              >
+                {place.name}
+              </div>
             </div>
           </div>
         ))}
-        <br/>
+        <br />
       </div>
     </div>
   );
